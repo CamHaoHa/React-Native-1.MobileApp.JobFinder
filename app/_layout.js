13a820a19c01fa1ab983ddba1f54ff1ef5c34874
CamHaoHa <camhaovn@gmail.com>
2023-04-18 22:51:27 +1000

    feat: useCallback to ensure all fonts are loaded correctly before render View

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,25 @@
 import { Stack } from 'expo-router';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 function Layout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+    DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+    DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+  });
+
+  const onLayOutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+  return <Stack onLayout={onLayOutRootView} />;
 }
 
 export default Layout;
