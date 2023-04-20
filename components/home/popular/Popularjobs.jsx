d21e16aeee89d88577032a561ab2d7e8ff79e1d0
CamHaoHa <camhaovn@gmail.com>
2023-04-20 12:56:12 +1000

    ui/ix for searching popular jobs

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,14 +1,51 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { useState } from 'react';
 
-import styles from './popularjobs.style'
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native';
 
+import { SIZES, COLORS } from '../../../constants';
+import styles from './popularjobs.style';
+
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
 const Popularjobs = () => {
-  return (
-    <View>
-      <Text>Popularjobs</Text>
-    </View>
-  )
-}
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-export default Popularjobs
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show All</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator
+            size="large"
+            color={COLORS.primary}
+          />
+        ) : error ? (
+          <Text>Oop... Something Went Wront!</Text>
+        ) : (
+          <FlatList
+            data={[1, 2, 3, 4, 5]}
+            renderItem={({ item }) => <PopularJobCard item={item} />}
+            keyExtractor={(item) => item?.job_id}
+            horizontal
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+          />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Popularjobs;
